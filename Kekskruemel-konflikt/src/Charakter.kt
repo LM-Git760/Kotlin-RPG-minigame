6a752fc574f5497ede29a6dd9dde3d60d05c6ed7
Florian Schur <florischur@googlemail.com>
2023-07-11 11:13:24 +0200

    Basis Klasse Charakter

--- Kekskruemel-konflikt/src/Charakter.kt
+++ Kekskruemel-konflikt/src/Charakter.kt
@@ -1,20 +1,3 @@
-open class Charakter(
-    var gesundheit: Int = 100,
-
-    var kraft: Int = 35,
-    var heilung: Int = 40
-){
-
-
-
+abstract class Charakter(val name: String, var hp: Int, var schaden: Double) {
+    abstract fun aktion(): Aktion
 }
-
-
-     fun angreifen(gegner: Gegner, charakter: Charakter){
-        gegner.gesundheit -= charakter.kraft
-    }
-    fun heilung(charakter: Charakter){
-        charakter.gesundheit += charakter.heilung
-        println(" Neue Gesundheit: \u001B[32m\u001B[1m${charakter.gesundheit} LP\u001B[0m")
-    }
-
