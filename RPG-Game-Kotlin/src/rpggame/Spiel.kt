de5f0a738a4a6c86f640b20eb4b903b5e07785ea
Florian Schur <florischur@googlemail.com>
2023-07-13 17:18:15 +0200

    Konsolen ausgabe optisch angepasst.

--- RPG-Game-Kotlin/src/rpggame/Spiel.kt
+++ RPG-Game-Kotlin/src/rpggame/Spiel.kt
@@ -6,15 +6,15 @@ class Spiel(var helden: MutableList<Held>, var gegner: MutableList<Gegner>) {
 
         // Ausgabe der Helden und Gegner zu Beginn des Spiels
         helden.forEach { held -> println("${held.name} (${held.hp} HP) tritt auf das Spielfeld!") }
-        Thread.sleep(800)
+        Thread.sleep(1500)
         gegner.forEach { gegner -> println("${gegner.name} (${gegner.hp} HP) wurde auf dem Spielfeld gespawnt!") }
-        Thread.sleep(800)
+        Thread.sleep(1500)
 
         while (helden.any { it.hp > 0 } && gegner.any { it.hp > 0 }) {
             // Helden sind am Zug
             for (held in helden) {
                 if (held.hp > 0 && gegner.any {it.hp > 0}) {
-                    println("${held.name}(${held.hp}HP) ist am Zug")
+                    println("${held.name}(${held.hp}HP) ist am Zug\n")
                     val aktion = held.waehleAktion()
                     val ziel = held.waehleZiel(gegner.filter { it.hp > 0 })
                     Thread.sleep(2000)
@@ -33,11 +33,12 @@ class Spiel(var helden: MutableList<Held>, var gegner: MutableList<Gegner>) {
                     val aktion = gegner.waehleAktion()
                     val ziel = gegner.waehleZiel(helden.filter { it.hp > 0 })
                     aktion.ausfuehren(gegner, ziel)
-                    println("${gegner.name} (${gegner.hp}HP) hat ${ziel.name} (${ziel.hp}HP) mit ${aktion.name} angegriffen!")
+                    println("${gegner.name} (${gegner.hp}HP) hat ${ziel.name} (${ziel.hp}HP) mit ${aktion.name} angegriffen! \n")
                     if (ziel.hp <= 0) {
                         println("${ziel.name} ist tot.")
                         helden.remove(ziel)
                     }
+                    println("---------------------------------")
                     Thread.sleep(850)
                 }
             }
